c30a0294569bd73014242060e9d2a43c7e8e9286
shjonz <shjonathantan11@gmail.com>
2023-03-08 22:48:35 +0800

    added new searchview bar in activity_maps.xml, added new function to MapsActivity.java, now can search for locations but got no search bar

--- app/src/main/java/com/example/fcctut/MapsActivity.java
+++ app/src/main/java/com/example/fcctut/MapsActivity.java
@@ -1,7 +1,10 @@
 package com.example.fcctut;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.SearchView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,24 +15,59 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
-    //private ActivityMapsBinding binding;
+    private GoogleMap mMap; //for map fragment physical map
+    private SearchView searchView; //searchView is the text bar that allows u to search for locations on the maps page
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.d("maps", "i am in maps create");
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        setContentView(R.layout.activity_maps); //to view map fragment
 
-        //binding = ActivityMapsBinding.inflate(getLayoutInflater());
-        //setContentView(binding.getRoot());
+        searchView = findViewById(R.id.search_view_location); //this is to get search view location that user entered in the search bar
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        //mapFragment.getMapAsync(this); //this code is to get map fragment for the map to display
+
+
+        //now below this is gonna be a function that listens for the text user inputs into the search bar to enter location
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                String location = searchView.getQuery().toString();
+                List<Address> addressList=null;
+
+                if (location != null || !location.equals("")) {
+                    Geocoder geocoder = new Geocoder((MapsActivity.this));
+                    try {
+                        addressList=geocoder.getFromLocationName(location, 1);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    Address address = addressList.get(0);
+                    LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+                    mMap.addMarker(new MarkerOptions().position(latLng).title(location));
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+                }
+                return false;
+            }
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        }); //end of function for listening to search bar input
+        mapFragment.getMapAsync(this); //this will update the map.
+
+
+
     }
 
     /**
@@ -46,8 +84,8 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //LatLng sydney = new LatLng(-34, 151);
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
