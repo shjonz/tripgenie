637790e30530124f7266d4bbcc401b667af21084
felic <felicia_jow@yahoo.com>
2023-03-10 15:28:44 +0800

    add 3rd button

--- app/src/main/java/com/example/fcctut/MainActivity.java
+++ app/src/main/java/com/example/fcctut/MainActivity.java
@@ -2,35 +2,50 @@ package com.example.fcctut;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView; //import textview from widget package into java file. to get classes for UI elements
 
 public class MainActivity extends AppCompatActivity {
+//    String path;
+//    Uri url;
+//    private ImageView captureimage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); //setting to activity main file this java class related to activity main layout file.
-
+        setContentView(R.layout.homepage);//setting to activity main file this java class related to activity main layout file.
     }
+//        captureimage=findViewById(R.id.setting_profile_image);{
+//            public void onClick(View V){
+//                ImagePicker.Companion.with(imageActivity.this)
+//                        .crop()
+//                        .maxResultSize(1080,1080)
 
-    public void onBtnClick (View view) {
-        //class obj_name
-        TextView txtFirstName = findViewById(R.id.firstname); //this is to find diff views/diff UIs elements from yr layout files
-        //R stands for resources (our static files in our project) txtMessage is the id in our layout xml file.
-        TextView txtLastName = findViewById(R.id.lastname);
-        TextView txtEmail = findViewById(R.id.email);
+//    public void addtripbutton(View view){
+//        setContentView(R.layout.homepage);
+//    }
 
-        EditText editEmail = findViewById(R.id.editemail);
-        EditText edtLastName = findViewById(R.id.editlastname);
-        EditText edtFirstName = findViewById(R.id.editFirstName);
 
-        txtFirstName.setText("First Name: "+edtFirstName.getText().toString() );
-        txtLastName.setText("Last Name: "+edtLastName.getText().toString());
-        txtEmail.setText("Email: "+editEmail.getText().toString());
-    }
+
+//    public void onBtnClick (View view) {
+//        //class obj_name
+//        TextView txtFirstName = findViewById(R.id.firstname); //this is to find diff views/diff UIs elements from yr layout files
+//        //R stands for resources (our static files in our project) txtMessage is the id in our layout xml file.
+//        TextView txtLastName = findViewById(R.id.lastname);
+//        TextView txtEmail = findViewById(R.id.email);
+//
+//        EditText editEmail = findViewById(R.id.editemail);
+//        EditText edtLastName = findViewById(R.id.editlastname);
+//        EditText edtFirstName = findViewById(R.id.editFirstName);
+//
+//        txtFirstName.setText("First Name: "+edtFirstName.getText().toString() );
+//        txtLastName.setText("Last Name: "+edtLastName.getText().toString());
+//        txtEmail.setText("Email: "+editEmail.getText().toString());
+//    }
 
 
 }
