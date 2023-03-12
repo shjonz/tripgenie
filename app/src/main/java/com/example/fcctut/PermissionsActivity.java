fdc436ff647f33eb68da214b64ec2f8e718bf247
cobrakwa <79395492+cobrakwa@users.noreply.github.com>
2023-03-12 15:53:08 +0800

    add Current Location Finder and permission request upon entering loading maps page

--- app/src/main/java/com/example/fcctut/PermissionsActivity.java
+++ app/src/main/java/com/example/fcctut/PermissionsActivity.java
@@ -1,29 +1,26 @@
 package com.example.fcctut;
 
 import android.Manifest;
-        import android.content.DialogInterface;
-        import android.content.Intent;
-        import android.content.pm.PackageManager;
-        import android.net.Uri;
-        import android.provider.Settings;
-//        import android.support.v4.content.ContextCompat;
-//        import android.support.v7.app.AlertDialog;
-//        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.view.View;
-        import android.widget.Button;
-        import android.widget.Toast;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import com.karumi.dexter.Dexter;
-        import com.karumi.dexter.PermissionToken;
-        import com.karumi.dexter.listener.PermissionDeniedResponse;
-        import com.karumi.dexter.listener.PermissionGrantedResponse;
-        import com.karumi.dexter.listener.PermissionRequest;
-        import com.karumi.dexter.listener.single.PermissionListener;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 public class PermissionsActivity extends AppCompatActivity {
     private Button btnGrant;
