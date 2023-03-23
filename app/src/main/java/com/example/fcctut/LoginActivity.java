610d9546e193269621ed6407ffe543b7ee7fe943
felic <felicia_jow@yahoo.com>
2023-03-23 19:43:46 +0800

    added edituserpage xml,signuppage xml, edited profile,strings added dependencies

--- app/src/main/java/com/example/fcctut/LoginActivity.java
+++ app/src/main/java/com/example/fcctut/LoginActivity.java
@@ -1,15 +1,108 @@
 package com.example.fcctut;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.internal.zabe;
+import com.google.android.gms.signin.internal.SignInClientImpl;
+import com.google.android.gms.tasks.Task;
 
 public class LoginActivity extends AppCompatActivity {
+    TextView signuptextview;
+    Button loginbutton;
+
+    Button signingooglebutton;
+
+    GoogleSignInOptions googleSignInOptions;
+    GoogleSignInClient googleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loginpage);
+
+        signingooglebutton=findViewById(R.id.signingooglebutton);
+
+        googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
+
+        signingooglebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SignIn();
+            }
+        });
+
+
+
+
+        loginbutton=findViewById(R.id.loginbutton);
+
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this,ProfileActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+        TextView signup = (TextView) findViewById(R.id.signuptextview);
+        signup.setOnClickListener(
+                new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v){
+                        Intent intent = new Intent (LoginActivity.this, SignUpPage.class);
+                                startActivity(intent);
+                    }
+                }
+        );
+    }
+
+    private void SignIn() {
+        Intent intent = googleSignInClient.getSignInIntent();
+        startActivityForResult(intent,100);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode,@Nullable Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+
+        if(requestCode==100){
+            Task<GoogleSignInAccount> task=GoogleSignIn.getSignedInAccountFromIntent(data);
+            try{
+                task.getResult(ApiException.class);
+                ProfileActivity2();
+            } catch (ApiException e){
+                Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    private void ProfileActivity2(){
+
+        finish();
+        Intent intent = new Intent(getApplicationContext(),ProfileActivity2.class);
+        startActivity(intent);
+
+
     }
 
 } //end of login activity class
